7a89746b062339de37596beda27c74ab101122e5
Caue Waneck <waneck@gmail.com>
2021-06-21 18:41:24 -0300

    Add `claimGameServer`

--- src/js/npm/aws_sdk/GameLift.hx
+++ src/js/npm/aws_sdk/GameLift.hx
@@ -701,6 +701,63 @@ extern class GameLift extends Service {
   @:overload(function():Request {})
   @:overload(function(callback:GameLiftError->{ MatchmakingTicket:MatchmakingTicket }->Void):Request {})
   function startMatchBackfill(params:StartMatchBackfillRequest, callback:GameLiftError->{ MatchmakingTicket:MatchmakingTicket }->Void):Request;
+
+  /**
+    Locates an available game server and temporarily reserves it to host gameplay
+    and players. This operation is called from a game client or client service (such
+    as a matchmaker) to request hosting resources for a new game session. In
+    response, GameLift FleetIQ locates an available game server, places it in
+    CLAIMED status for 60 seconds, and returns connection information that players
+    can use to connect to the game server.
+
+    To claim a game server, identify a game server group. You can also specify a
+    game server ID, although this approach bypasses GameLift FleetIQ placement
+    optimization. Optionally, include game data to pass to the game server at the
+    start of a game session, such as a game map or player information.
+
+    When a game server is successfully claimed, connection information is returned.
+    A claimed game server's utilization status remains AVAILABLE while the claim
+    status is set to CLAIMED for up to 60 seconds. This time period gives the game
+    server time to update its status to UTILIZED (using UpdateGameServer) once
+    players join. If the game server's status is not updated within 60 seconds, the
+    game server reverts to unclaimed status and is available to be claimed by
+    another request. The claim time period is a fixed value and is not
+    configurable.
+
+    If you try to claim a specific game server, this request will fail in the
+    following cases:
+
+    If the game server utilization status is UTILIZED.
+
+    If the game server claim status is CLAIMED.
+  **/
+  function claimGameServer(params:{
+    /**
+      A unique identifier for the game server group where the game server is running. Use either the GameServerGroup name or ARN value.
+      If you are not specifying a game server to claim, this value identifies where you want GameLift FleetIQ to look for an available game server to claim.
+    **/
+    GameServerGroupName:String,
+    /**
+      A set of custom game server properties, formatted as a single string value. This data is passed to a game client or service when it requests information
+      on game servers using ListGameServers or ClaimGameServer.
+    **/
+    ?GameServerData:String,
+    ?GameServerId:String,
+  }, callback:GameLiftError->{ GameServer:FleetIqGameServer }->Void):Request;
+}
+
+typedef FleetIqGameServer = {
+  ClaimStatus:String,
+  ConnectionInfo:String,
+  GameServerData:String,
+  GameServerGroupArn:String,
+  GameServerGroupName:String,
+  GameServerId:String,
+  InstanceId:String,
+  LastClaimTime:Float,
+  LastHealthCheckTime:Float,
+  RegistrationTime:Float,
+  UtilizationStatus:String
 }
 
 typedef StartMatchBackfillRequest = {
